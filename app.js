8099b0e03e29f31e46c1b84eac2420ba50845dd1
Pushkar <bansalpushkar100@gmail.com>
2022-11-01 22:26:50 +0530

    mailing in admin

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ const forgot = require('./routes/Login/forgot')
 const changePass = require('./routes/Login/changePass')
 const admin = require('./routes/Admin')
 //
-const {loginRequired} = require('./utils/loginMiddleware')
-const wrapAsync = require('./utils/wrapAsync')
 const methodOverride = require('method-override')
 const flash = require('connect-flash');
 const ejsMate = require('ejs-mate')
@@ -76,34 +74,6 @@ app.use('/test',tests)
 app.use('/client',clients)
 //admin
 app.use('/admin',admin)
-
-
-// ADMIN ROUTES
-
-// app.get('/admin',loginRequired('admin'),wrapAsync(async(req,res)=>{
-//     const users = await User.find({})
-//     res.render('admin.ejs',{users})
-// }))
-
-// app.put('/admin/user/:id',wrapAsync(async(req,res)=>{
-//     const {id} = req.params
-//     const {email} = req.body
-//     const unique = await User.findOne({email})
-//     if(unique){
-//         req.flash('error',"Email already in use")
-//         res.redirect('/admin')
-//     }else{
-//         const user = await User.findById(id)
-//         user.email = email
-//         if(req.body.password){
-//             user.password = password
-//         }
-//         await user.save()
-
-//         req.flash('success','Email Changed Successfully')
-//         res.redirect('/admin')
-//     }
-// }))
 
 app.all('*',(req,res)=>{
    throw new AppError('Page Not found',404)
--- routes/Admin/index.js
+++ routes/Admin/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router({mergeParams:true})
 const {loginRequired} = require('../../utils/loginMiddleware')
 const User = require('../../models/User')
+const transporter = require('../../utils/nodeMailer')
 const wrapAsync = require('../../utils/wrapAsync')
 
 router.route('/')
@@ -16,6 +17,7 @@ router.route('/user/:id')
     const {email,password} = req.body
     const unique = await User.findOne({email})
     const user = await User.findById(id)
+    const previousMail = user.email
     if(unique){
         if(password && unique.email == user.email){
             unique.password = password
@@ -24,38 +26,33 @@ router.route('/user/:id')
             res.redirect('/admin')
         }else{
             req.flash('error',"Email already in use")
-            res.redirect('/admin')
+            return res.redirect('/admin')
         }
-    }else{        
+    }else{
         user.email = email
         if(req.body.password){
             user.password = password
+            req.flash('success','Email and password changed Successfully')
+        }else{
+            req.flash('success','Email Changed Successfully')
         }
         await user.save()
-
-        req.flash('success','Email Changed Successfully')
         res.redirect('/admin')
     }
+    const admin = await User.findOne({position:"admin"})
+    const mailOptions = {
+        from: process.env.MAIL_ADDRESS,
+        to: admin.email,
+        subject: "Changes in user details",
+        html:`Details of following user were changed: <br><br>
+        Name: ${user.name}<br><br>
+        Previous Email: ${unique ? user.email : previousMail}<br><br>
+        Current Email: ${user.email}<br><br>
+        Position: ${user.position}<br><br>
+        ${password ? `Password: ${password}<br><br>` : `Password was not changed`}
+        `
+    };   
+    transporter.sendMail(mailOptions); 
 }))
-
-// .put(loginRequired('admin'),wrapAsync(async(req,res)=>{
-//     const {id} = req.params
-//     const {email} = req.body
-//     const unique = await User.findOne({email})
-//     if(unique){
-//         req.flash('error',"Email already in use")
-//         res.redirect('/admin')
-//     }else{
-//         const user = await User.findById(id)
-//         user.email = email
-//         if(req.body.password){
-//             user.password = password
-//         }
-//         await user.save()
-
-//         req.flash('success','Email Changed Successfully')
-//         res.redirect('/admin')
-//     }
-// }))
 
 module.exports = router
