f12fe8bc14a8d71d38a15bfa0f33e3ade67065e7
Pushkar <bansalpushkar100@gmail.com>
2022-11-05 14:40:59 +0530

    adding test to dept

--- routes/Manager/test.js
+++ routes/Manager/test.js
@@ -18,17 +18,31 @@ router.route('/all')
         city = 'VAD'
     }
     const tests = await Test.find({}).populate(`dept${city}`)
-    res.render('manager/all-tests.ejs',{tests})
+    const departments = await Department.find({city:req.session.city})
+    res.render('manager/all-tests.ejs',{tests,city,departments})
 }))
 
 router.route('/:testId/dept/:deptId')
 .post(loginRequired('manager'),wrapAsync(async(req,res)=>{
     const {testId,deptId} = req.params
-    const dept = await Department.findByIdAndUpdate(deptId,{$push:{tests:testId}})
-    const test = await Test.findById(testId)
-    test['dept'+req.session.city] = deptId
-    await test.save()
-    req.flash('success',"department assigned to test")
+    let city
+    if(req.session.city == 'CITY - 2'){
+        city = '2'
+    } else if(req.session.city == 'CITY - 3'){
+        city = '3'
+    }else{
+        city = 'VAD'
+    }
+    await Department.findOneAndUpdate({tests:testId},{$pull:{tests:testId}})
+    if(deptId == 'none'){
+        await Test.findByIdAndUpdate(testId,{$unset:{['dept'+city]:1}})
+    }else{        
+        const dept = await Department.findByIdAndUpdate(deptId,{$push:{tests:testId}})
+        const test = await Test.findById(testId)
+        test['dept'+city] = deptId
+        await test.save()
+    }
+    req.flash('success',"Department Changed Successfully")
     res.redirect("/test/all")
 }))
 
